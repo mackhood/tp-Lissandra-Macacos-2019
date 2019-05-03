97ed81fdbddbf578993fa4bc07787e006b2bd3c6
Dragonflares <ivanlose@hotmail.com>
2019-05-03 15:42:17 -0300

    Testo de deploy

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,19 @@
 cd
 mkdir workspace
-cd 
+cd
 mv tp-2019-1c-Macacos/ workspace/
 cd
 
-git clone https://github,com/sisoputnfrba/so-commons-library.git
-cd so-commons-library
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd so-commons-library/
 sudo make install
 cd
 
+
 cd /home/utnso/workspace/tp-2019-1c-Macacos/SharedLibrary/Debug/
 sudo make
-export LD_LIBRARY_PATH=/home/utnso/workspace/tp-2019-1c-Macacos/SharedLibrary/debug
-cd
-
-cd /home/utnso/workspace/tp-2019-1c-Macacos/Kernel/Debug/
-make clean
-make all
-cd
+export LD_LIBRARY_PATH=/home/utnso/workspace/tp-2019-1c-Macacos/SharedLibrary/Debug/
+cd 
 
 cd /home/utnso/workspace/tp-2019-1c-Macacos/LissandraFilesystem/Debug/
 make clean
@@ -26,6 +22,13 @@ cd
 
 cd /home/utnso/workspace/tp-2019-1c-Macacos/Memoria/Debug/
 make clean
-make all 
+make all
 cd
+
+cd /home/utnso/workspace/tp-2019-1c-Macacos/Kernel/Debug/
+make clean
+make all
+cd
+
+
 
