11891127ceed0840df5d1d10ba1d82c1d4d4ebac
Martin Olivera <jm98olivera@gmail.com>
2019-07-13 14:48:30 -0300

    Saco hilos de aceptar clientes

--- Memoria/src/TimerJournal.c
+++ Memoria/src/TimerJournal.c
@@ -3,6 +3,7 @@
 void journalInTime(){
 	while(1){
 		usleep(info_memoria.tiempo_jour *1000);
+		printf("Se procedera a realizar el journal debido a su timer");
 		pthread_mutex_lock(&mutex_estructuras_memoria);
 		journalReq();
 		pthread_mutex_unlock(&mutex_estructuras_memoria);
